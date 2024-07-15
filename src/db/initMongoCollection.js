8ad51b5ff808b4c24476aa85376461832969b5fb
Serhii Balaba <balabaserhii@gmail.com>
2024-07-15 17:19:19 +0200

    Update initMongoCollection.js

--- src/db/initMongoCollection.js
+++ src/db/initMongoCollection.js
@@ -3,17 +3,14 @@ import { env } from '../utils/env.js';
 
 export const initMongoConnection = async () => {
   try {
-    // const user = env('MONGODB_USER');
-    // const pwd = env('MONGODB_PASSWORD');
-    // const url = env('MONGODB_URL');
-    // const db = env('MONGODB_DB');
-    const DB = env('DB_URI');
+    const user = env('MONGODB_USER');
+    const pwd = env('MONGODB_PASSWORD');
+    const url = env('MONGODB_URL');
+    const db = env('MONGODB_DB');
 
-    await mongoose.connect(DB);
-
-    // await mongoose.connect(
-    //   `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
-    // );
+    await mongoose.connect(
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
+    );
 
     console.log('Mongo connection successfully established!');
   } catch (e) {
