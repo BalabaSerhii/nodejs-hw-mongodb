9f86186e16c18199f90db1fff9343874e51e064c
Serhii Balaba <balabaserhii@gmail.com>
2024-08-01 16:05:29 +0200

    с

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -1,10 +1,10 @@
-import { isValidObjectId } from 'mongoose';
-import createHttpError from 'http-errors';
+import { HttpError } from 'http-errors';
 
 export const isValidId = (req, res, next) => {
   const { contactId } = req.params;
   if (!isValidObjectId(contactId)) {
-    return next(createHttpError(404, 'Not found'));
+    throw new HttpError(404, 'Not found');
   }
+
   next();
 };
--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -2,5 +2,4 @@ export const notFoundHandler = (req, res, next) => {
   res.status(404).json({
     message: 'Route not found',
   });
-  next();
 };
