f0f1d5298207dbbdc697a019a27a81a9f0645a38
Serhii Balaba <balabaserhii@gmail.com>
2024-08-02 07:33:22 +0200

    c

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,19 +1,3 @@
-// import createHttpError from 'http-errors';
-
-// export const validateBody = (schema) => async (req, res, next) => {
-//   try {
-//     await schema.validateAsync(req.body, {
-//       abortEarly: false,
-//     });
-//     next();
-//   } catch (err) {
-//     const error = createHttpError(400, 'Bad Request', {
-//       errors: err.details,
-//     });
-//     next(error);
-//   }
-// };
-
 export const validateBody = (schema) => async (req, res, next) => {
   try {
     await schema.validateAsync(req.body, {
@@ -25,3 +9,6 @@ export const validateBody = (schema) => async (req, res, next) => {
     next(err);
   }
 };
+
+
+
