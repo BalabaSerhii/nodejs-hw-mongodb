04a3da2a1d4196c79bb59560c04984b9e4ff509a
Serhii Balaba <balabaserhii@gmail.com>
2024-08-13 16:32:16 +0200

    x

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -5,90 +5,73 @@ import {
   refreshUsersSession,
   logoutUser,
 } from '../services/auth.js';
-import createHttpError from 'http-errors';
 
-const setupSessionCookies = (res, session, expiresIn = ONE_DAY * 30) => {
-  const options = {
+export const registerUserController = async (req, res) => {
+  const user = await registerUser(req.body);
+
+  res.json({
+    status: 201,
+    message: 'Successfully registered a user!',
+    data: user,
+  });
+};
+
+export const loginUserController = async (req, res) => {
+  const session = await loginUser(req.body);
+
+  res.cookie('refreshToken', session.refreshToken, {
     httpOnly: true,
-    expires: new Date(Date.now() + expiresIn),
-  };
+    expires: new Date(Date.now() + ONE_DAY),
+  });
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
 
-  res.cookie('refreshToken', session.refreshToken, options);
-  res.cookie('sessionId', session._id, options);
+  res.json({
+    status: 200,
+    message: 'Successfully logged in an user!',
+    data: {
+      accessToken: session.accessToken,
+    },
+  });
 };
 
-export const registerUserController = async (req, res, next) => {
-  try {
-    const user = await registerUser(req.body);
-
-    res.status(201).json({
-      status: 201,
-      message: 'Successfully registered a user!',
-      data: user,
-    });
-  } catch (error) {
-    next(
-      createHttpError(500, 'Failed to register user', { data: error.message }),
-    );
-  }
+const setupSession = (res, session) => {
+  res.cookie('refreshToken', session.refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY * 30),
+  });
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY * 30),
+  });
 };
 
-export const loginUserController = async (req, res, next) => {
-  try {
-    const session = await loginUser(req.body);
+export const refreshUserSessionController = async (req, res) => {
+  const session = await refreshUsersSession({
+    sessionId: req.cookies.sessionId,
+    refreshToken: req.cookies.refreshToken,
+  });
 
-    setupSessionCookies(res, session, ONE_DAY);
+  setupSession(res, session);
 
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully logged in a user!',
-      data: {
-        accessToken: session.accessToken,
-      },
-    });
-  } catch (error) {
-    next(createHttpError(401, 'Login failed', { data: error.message }));
-  }
+  res.json({
+    status: 200,
+    message: 'Successfully refreshed a session!',
+    data: {
+      accessToken: session.accessToken,
+    },
+  });
 };
 
-export const refreshUserSessionController = async (req, res, next) => {
-  try {
-    const session = await refreshUsersSession({
-      sessionId: req.cookies.sessionId,
-      refreshToken: req.cookies.refreshToken,
-    });
-
-    setupSessionCookies(res, session);
-
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully refreshed a session!',
-      data: {
-        accessToken: session.accessToken,
-      },
-    });
-  } catch (error) {
-    next(
-      createHttpError(401, 'Failed to refresh session', {
-        data: error.message,
-      }),
-    );
+export const logoutUserController = async (req, res) => {
+  if (req.cookies.sessionId) {
+    await logoutUser(req.cookies.sessionId);
   }
-};
 
-export const logoutUserController = async (req, res, next) => {
-  try {
-    const { sessionId } = req.cookies;
+  res.clearCookie('sessionId');
+  res.clearCookie('refreshToken');
 
-    if (sessionId) {
-      await logoutUser(sessionId);
-    }
-
-    res.clearCookie('sessionId');
-    res.clearCookie('refreshToken');
-
-    res.status(204).send();
-  } catch (error) {
-    next(createHttpError(500, 'Failed to logout', { data: error.message }));
-  }
+  res.status(204).send();
 };
